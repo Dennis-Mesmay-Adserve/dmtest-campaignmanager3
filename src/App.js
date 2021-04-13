7078a58da20516cdcb92145d7ab49579085f78f6
Dennis Mesmay <dennis.mesmay@adserve.com>
2021-04-13 10:36:59 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Edited by Dennis <code>version 1.01</code>
+          Edited by Dennis <code>version 1.02</code>
         </p>
         <a
           className="App-link"
